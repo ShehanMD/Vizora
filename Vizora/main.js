89e2ec56348b91cbab4e86e6b45cc975cef6ad77
Shehan Maduwantha <www.sm16252@gmail.com>
2025-04-14 17:30:34 +0530

    added size changing option

--- Vizora/main.js
+++ Vizora/main.js
@@ -5,7 +5,14 @@ function getPrompt() {
     let prompt = document.getElementById("prompt").value;
     console.log(prompt); // Output in console
     alert(prompt); // Show in alert box
-    let url = `https://stabledifffusion.com/imagen/${encodeURIComponent(prompt)}?width=1024&height=1024`;
-    window.location.href = url; 
+    let width = document.getElementById("widthIMG").value;
+    let height = document.getElementById("heightIMG").value;
+    if (width.value & height.value == 0) {
+        alert("Plese enter Width & Height");
+        let url = `#`;
+        window.location.href = url;
+    }
+    let url = `https://stabledifffusion.com/imagen/${encodeURIComponent(prompt)}?width=${encodeURIComponent(width)}&height=${encodeURIComponent(height)}`;
+    window.location.href = url;
 
 }
