910603ab2818af73dcc0555ea42326b59ea24ac8
Shehan Maduwantha <www.sm16252@gmail.com>
2025-04-22 16:23:24 +0530

    made open image in currunt page and added a close button

--- Vizora/main.js
+++ Vizora/main.js
@@ -1,18 +1,25 @@
 let genarate = document.getElementById("genarate")
-
+let url;
 
 function getPrompt() {
     let prompt = document.getElementById("prompt").value;
     console.log(prompt); // Output in console
-    alert(prompt); // Show in alert box
+   
     let width = document.getElementById("widthIMG").value;
     let height = document.getElementById("heightIMG").value;
     if (width.value & height.value == 0) {
         alert("Plese enter Width & Height");
-        let url = `#`;
+        url = `#`;
         window.location.href = url;
     }
     let url = `https://stabledifffusion.com/imagen/${encodeURIComponent(prompt)}?width=${encodeURIComponent(width)}&height=${encodeURIComponent(height)}`;
-    window.location.href = url;
+    
+
+    document.getElementById("ImageResalts").src=url
+    document.getElementById("loder").style.display="block"
+    
+    
+
 
 }
+
