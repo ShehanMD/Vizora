e9f22a86e212c857471e497639f88183fe8d291b
Shehan Maduwantha <www.sm16252@gmail.com>
2025-07-21 19:05:54 +0530

    Changed API server to "pollinations.ai"

--- Vizora/main.js
+++ Vizora/main.js
@@ -5,14 +5,8 @@ function getPrompt() {
     let prompt = document.getElementById("prompt").value;
     console.log(prompt); // Output in console
    
-    let width = document.getElementById("widthIMG").value;
-    let height = document.getElementById("heightIMG").value;
-    if (width.value & height.value == 0) {
-        alert("Plese enter Width & Height");
-        url = `#`;
-        window.location.href = url;
-    }
-    let url = `https://stabledifffusion.com/imagen/${encodeURIComponent(prompt)}?width=${encodeURIComponent(width)}&height=${encodeURIComponent(height)}`;
+    
+    url = `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`;
     
 
     document.getElementById("ImageResalts").src=url
